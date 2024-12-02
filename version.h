1b798ac0677bf2405dff06b2ae019cb3a7646cfb
JulienML <julien-devos@live.fr>
2024-12-02 22:10:55 +0100

    v3.0.3

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #pragma once
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 0
-#define VERSION_PATCH 2
-#define VERSION_BUILD 415
+#define VERSION_PATCH 3
+#define VERSION_BUILD 416
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
